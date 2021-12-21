b5d0e6c1aa2da775193afc2f5db32317623823b6
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2021-12-21 15:08:03 -0500

    added logout

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Products from './components/Prodcuts'
 import Product from './components/Product'
-import {Routes, Route} from 'react-router-dom'
-
+import {Switch, Route} from 'react-router-dom'
+import PrivateRoute from './components/PrivateRoute'
+import Logout from './components/Logout';
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route path='register' element={<Register/>}/>
-        <Route path='/products' element={<Products/>}/>
-        <Route path='/products/:id' element={<Product/>}/>
-      </Routes>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/register' component={Register}/>
+        <PrivateRoute path='/products' component={Products}/>
+        <PrivateRoute path='/products/:id' component={Product}/>
+        <PrivateRoute path='/logout' component={Logout}/>
+      </Switch>
     </div>
   );
 }
